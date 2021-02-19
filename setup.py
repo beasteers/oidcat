7366077c14a7c2da8b817e155d35afd5a896801d
Bea <bea.steers@gmail.com>
2021-02-19 11:57:48 -0500

    bump 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ NAME = 'oidcat'
 
 setuptools.setup(
     name=NAME,
-    version='0.0.6',
+    version='0.1.0',
     description='easy oidc client & server',
     long_description=open('README.md').read().strip(),
     long_description_content_type='text/markdown',
